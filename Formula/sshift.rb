85a5ed4803d14cd0eee93a3d2cee2efd28357006
takealook97 <takealook97@naver.com>
2025-07-14 12:42:32 +0900

    Update Homebrew formula to v1.3.4 (English only)

--- Formula/sshift.rb
+++ Formula/sshift.rb
@@ -1,59 +1,24 @@
 class Sshift < Formula
   desc "SSH server management tool with jump server support"
-  desc "SSH server management tool with jump server support"
   homepage "https://github.com/takealook97/sshift"
-  homepage "https://github.com/takealook97/sshift"
-  version "v1.3.3-dirty"
-  version "v1.3.3-dirty"
-  license "MIT"
+  version "1.3.4"
   license "MIT"
 
-
-  # Go source code
-  # Go source code
-  url "https://github.com/takealook97/sshift/archive/refs/tags/v1.3.3-dirty.tar.gz"
-  url "https://github.com/takealook97/sshift/archive/refs/tags/v1.3.3-dirty.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"https://github.com/takealook97/sshift/archive/refs/tags/v1.3.3-dirty.tar.gz" | shasum -a 256 | cut -d" " -f1)"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"https://github.com/takealook97/sshift/archive/refs/tags/v1.3.3-dirty.tar.gz" | shasum -a 256 | cut -d" " -f1)"
-
+  url "https://github.com/takealook97/sshift/archive/refs/tags/v1.3.4.tar.gz"
+  sha256 "b2f0ab58b95af62b4a493b0f5c6ef9cc3876ad0aa5aea34114fcdc9325fdb835"
 
   depends_on "go" => :build
-  depends_on "go" => :build
-
 
   def install
-  def install
-    # Set version from git tag
-    # Set version from git tag
-    ldflags = %W[
     ldflags = %W[
       -s -w
-      -s -w
-      -X main.Version=#{version}
       -X main.Version=#{version}
     ]
-    ]
-
-
-    system "go", "build", *std_go_args(ldflags: ldflags), "-o", bin/"sshift", "main.go"
     system "go", "build", *std_go_args(ldflags: ldflags), "-o", bin/"sshift", "main.go"
   end
-  end
-
 
   test do
-  test do
-    # Test version command
-    # Test version command
     assert_match "SSHift v#{version}", shell_output("#{bin}/sshift version")
-    assert_match "SSHift v#{version}", shell_output("#{bin}/sshift version")
-
-
-    # Test help command
-    # Test help command
-    assert_match "Usage:", shell_output("#{bin}/sshift help")
     assert_match "Usage:", shell_output("#{bin}/sshift help")
   end
-  end
-end
 end
