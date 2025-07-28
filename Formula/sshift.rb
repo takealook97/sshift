7ca93daddd0676d0e26bcd181c65c1757d827f76
takealook97 <takealook97@naver.com>
2025-07-28 12:59:44 +0900

    chore: Update Formula version to 2.0.0

--- Formula/sshift.rb
+++ Formula/sshift.rb
@@ -1,10 +1,10 @@
 class Sshift < Formula
   desc "SSH server management tool with jump server support"
   homepage "https://github.com/takealook97/sshift"
-  version "1.3.7"
+  version "2.0.0"
   license "MIT"
 
-  url "https://github.com/takealook97/sshift/archive/refs/tags/v1.3.7.tar.gz"
+  url "https://github.com/takealook97/sshift/archive/refs/tags/v2.0.0.tar.gz"
   sha256 "c31f9eae7c0cf7dd82155630b20515db90134ad7f2251e0201d31e043e9fd44e"
 
   depends_on "go" => :build
